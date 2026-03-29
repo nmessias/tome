5ceb6e702814d963661b4c21c9294e09d9aa516b
Nicolas Messias <nicolas.messias@kp-family.de>
2026-03-29 10:43:02 -0300

    chore: bump version to 1.4.0

--- src/config.ts
+++ src/config.ts
@@ -93,4 +93,4 @@ export const ALL_NAV_LINKS = [
 export const NAV_LINKS = ALL_NAV_LINKS;
 
 // App version for cache busting
-export const APP_VERSION = "1.3.8";
+export const APP_VERSION = "1.4.0";
